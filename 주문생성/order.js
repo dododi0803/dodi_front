035da33c0e554768ba3847abe3540bc1e1597b6c
dododi0803 <dododi0803@gmail.com>
2026-02-11 18:32:07 +0900

    20260211

--- 주문생성/order.js
+++ 주문생성/order.js
@@ -2,7 +2,20 @@ let cart = [];
 let total = 0;
 
 function addItem(name, price) {
-    cart.push({ name, price });
+
+    // 이미 장바구니에 있는지 확인
+    const existingItem = cart.find(item => item.name === name);
+
+    if (existingItem) {
+        existingItem.quantity += 1;   // 수량 증가
+    } else {
+        cart.push({
+            name: name,
+            price: price,
+            quantity: 1
+        });
+    }
+
     total += price;
     renderCart();
 }
@@ -14,38 +27,13 @@ function renderCart() {
     cart.forEach(item => {
         const div = document.createElement("div");
         div.className = "cart-item";
-        div.innerText = `${item.name} - ${item.price.toLocaleString()}원`;
+
+        div.innerText =
+            `${item.name} ${item.quantity}개 - ${(item.price * item.quantity).toLocaleString()}원`;
+
         cartList.appendChild(div);
     });
 
     document.getElementById("totalPrice").innerText =
         `총 금액: ${total.toLocaleString()}원`;
-}
-
-// 주문하기 → 결제 화면으로
-function goToPayment() {
-    if(cart.length === 0){
-        alert("메뉴를 선택해주세요!");
-        return;
-    }
-
-    document.getElementById("menuScreen").style.display = "none";
-    document.getElementById("paymentScreen").style.display = "block";
-    document.getElementById("payAmount").innerText = total.toLocaleString();
-}
-
-// 결제 완료
-function completePayment() {
-    const orderNo = "A-" + Math.floor(Math.random() * 1000);
-    document.getElementById("orderNumber").innerText =
-        `주문번호 ${orderNo} / 결제 완료`;
-
-    // 초기화
-    cart = [];
-    total = 0;
-    renderCart();
-
-    // 결제 화면 → 메뉴 화면으로
-    document.getElementById("paymentScreen").style.display = "none";
-    document.getElementById("menuScreen").style.display = "block";
 }
